87925decaaa4eca4f43811d6598ce67d3c15b173
mariame2204@gmail.com <mariame2204@gmail.com>
2022-04-25 00:55:09 +0200

    test1

--- src/server/Server.java
+++ src/server/Server.java
@@ -10,7 +10,7 @@ public class Server{
 
     public static void main (String[] args) {
         try { 
-            
+            //test
              IGestion g= (IGestion)new GestionImpl();
              LocateRegistry.createRegistry(1940); 
              
