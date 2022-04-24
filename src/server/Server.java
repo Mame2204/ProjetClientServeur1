5da9d8c951712e550345339b52e04dc591d5f461
mariame2204@gmail.com <mariame2204@gmail.com>
2022-04-24 23:56:37 +0200

    Test2

--- src/server/Server.java
+++ src/server/Server.java
@@ -5,20 +5,20 @@ import java.rmi.registry.LocateRegistry;
 
 public class Server{
 
-	public static void main(String args[]) { 
-		 try { 
-			 
-	    	  IGestion g= (IGestion)new GestionImpl();
-	    	  LocateRegistry.createRegistry(1940); 
-	    	  
-	    	  Naming.rebind("rmi://localhost:1940/gestion", g);
-	    	  System.err.println("Server ready"); 
-	    	  
-	      } catch (Exception e) { 
-	         System.err.println("Server exception: " + e.toString()); 
-	         e.printStackTrace(); 
-	      }
-	   
-	   }
+    public static void main(String args[]) { 
+        try { 
+            
+             IGestion g= (IGestion)new GestionImpl();
+             LocateRegistry.createRegistry(1940); 
+             
+             Naming.rebind("rmi://localhost:1940/gestion", g);
+             System.err.println("Server ready"); 
+             
+         } catch (Exception e) { 
+            System.err.println("Server exception: " + e.toString()); 
+            e.printStackTrace(); 
+         }
+      
+      }
 
 }
